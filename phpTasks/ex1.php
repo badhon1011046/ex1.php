f142c33e64e83261a407a509a4e45b8212593023
badhon1011046 <sahmedb1011046@gmail.com>
2026-02-02 21:01:15 +0200

    Add files via upload

---
+++ phpTasks/ex1.php
@@ -0,0 +1,47 @@
+<h3>Exercise 1: Getting Started with PHP - SHAHAREAR AHMED BADHON </h3>
+<?php
+echo "Hello world . My name is Badhon";
+?>
+<h3>TASK 3:Creating ex1.php in the same folder </h3>
+<?php
+echo "Hello world! My name is David";
+?>
+<?php
+$title ="php is interesting.";
+echo "<h4>$title</h4>";
+?>
+<h3>TASK 3: Student grades tables</h3>
+<?php
+$sg1=5;
+$sg2=4;
+$sg3=5;
+?>
+<table border="1" cellpadding="10">
+    <tr>
+        <th>S.n.</th>
+        <th>Name</th>
+        <th>Grade</th>
+</tr>
+<tr>
+    <td>1</td>
+    <td>JOHN</td>
+    <td><?php echo $sg1 ?></td>
+</tr>
+<tr>
+    <td>2</td>
+    <td>ALICE</td>
+    <td><?php echo $sg2 ?></td>
+</tr>
+<tr>
+    <td>3</td>
+    <td>BOB</td>
+    <td><?php echo $sg3 ?></td>
+</tr>
+</table>
+<h3>SCREENSHOT: VS CODE</h3>
+<img src="Screenshot (303).png" alt="Screenshot of development environment" width="600">
+<h3>SCREENSHOT: LOCAL HOST</h3>
+<img src="Screenshot (304).png" alt="Screenshot of development environment" width="600">
+<h3>SCREENSHOT: DOCKER</h3>
+<img src="Screenshot (305).png" alt="Screenshot of development environment" width="600">
+
---
+++ phpTasks/index.php
@@ -0,0 +1,3 @@
+<?php
+echo "Shaharear Ahmed Badhon";
+?>
